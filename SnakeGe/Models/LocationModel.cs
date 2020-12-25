489a750d0b20fc6be14f2afd70a33216543abb37
ydmy760 <63996173+ydmy760@users.noreply.github.com>
2020-12-25 19:30:07 +0800

    Add files via upload

---
+++ SnakeGe/Models/LocationModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnakeGe.Models
+{
+    public class LocationModel
+    {
+        public List<List<string>> snake { get; set; }
+    }
+}
---
+++ SnakeGe/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnakeGe.Models
+{
+    public class LoginModel
+    {
+        public string State { get; set; }
+        public bool IsLogin { get; set; }
+        
+    }
+    public class LogonModel {
+        public string Logon { get; set; }
+        public string id { get; set; }
+    }
+}
